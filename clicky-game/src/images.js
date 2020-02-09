400a3562e6bb2805b16c2c2b2c6cb64aa04e334a
Janet <Reid>
2020-02-09 12:05:27 +0800

    Images files set up

---
+++ clicky-game/src/images.js
@@ -0,0 +1,29 @@
+import flower1 from "./images/flower1.jfif";
+import flower2 from "./images/flower2.jfif";
+import flower3 from "./images/flower3.jfif";
+import flower4 from "./images/flower4.jfif";
+import flower5 from "./images/flower5.jfif";
+import flower6 from "./images/flower6.jfif";
+import flower7 from "./images/flower7.jfif";
+import flower8 from "./images/flower8.jfif";
+import flower9 from "./images/flower9.jfif";
+import flower10 from "./images/flower10.jfif";
+import flower11 from "./images/flower11.jfif";
+import flower12 from "./images/flower12.jfif";
+
+const images = [
+    flower1,
+    flower2,
+    flower3,
+    flower4,
+    flower5,
+    flower6,
+    flower7,
+    flower8,
+    flower9,
+    flower10,
+    flower11,
+    flower12
+];
+
+export default images;
